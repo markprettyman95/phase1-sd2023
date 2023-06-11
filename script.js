bd5561989645b2046532aa57a34684f7caa4a34f
Mark Prettyman <markiesmyname@Marks-iMac.lan>
2023-06-11 11:05:11 -0700

    adds more structure to JS cards

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
     const asanaCards = document.getElementsByID('asanaCards');
 
-    fetch()
+    fetch(`http://localhost:3000/asanas`)
+    .then(resp => {
+        if (!response.ok) {
+            throw new Error ('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        const asanas = data
+
+        asanas.forEach(asana => {
+            const card = document.createElement('div')
+            card.classList.add('card')
+
+            const h2 = document.createElement('h2')
+            h2.textContent = asana.name
+            card.appendChild(h2)
+
+            const img = document.createElement('img')
+            img.src = asana.image
+            img.classList.add('toy-avatar')
+            card.appendChild(p)
+
+            const button = document.createElement('button')
+            button.classList.add('learn-more-btn')
+            button.textConte4nt = 'Learn More!'
+            card.appendChild('button')
+
+        })
+        
+    })
 })
