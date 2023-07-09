b62640ebe261c4f3f5c8fb61bd00deece36337ab
Mark Prettyman <markiesmyname@Marks-iMac.lan>
2023-07-09 10:19:56 -0700

    Asana Directory returns a new menu of selectable buttons representing each Asana, which when clicked, closes the menu and displays the selected card in index id section of HTML

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.getElementById('menu')
     const videoOverlay = document.querySelector('.video-overlay')
     const menuButtons = document.querySelectorAll('.menu-button')
+    const asanaCards = document.getElementById('asanaCards')
 
     hamburgerBtn.addEventListener('click', () => {
         toggleMenu()
@@ -15,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 closeMenu()
             } else if (buttonText === 'Home') {
                 window.location.href = 'index.html'
+            } else if (buttonText === 'Asana Directory') {
+                displayAsanas()
             } else {
                 const targetID = button.textContent.toLowerCase().replace(' ', '-')
                 const targetElement = document.getElementById(targetID)
@@ -32,6 +35,70 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeMenu = () => {
         menu.classList.remove('show')
         videoOverlay.classList.remove('blur')
+    }
+
+    const displayAsanas = () => {
+        menuButtons.forEach(button => {
+            button.style.display = 'none'
+        })
+
+        fetch(`http://localhost:3000/asanas`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                return response.json()
+            })
+            .then(data => {
+                const asanas = data
+                asanaCards.innerHTML = ''
+    
+                asanas.forEach(asana => {
+                    const asanaButton = document.createElement('button')
+                    asanaButton.classList.add('menu-button')
+                    asanaButton.textContent = asana.name
+    
+                    asanaButton.addEventListener('click', () => {
+                        document.getElementById('index').innerHTML = ''
+
+                        const card = document.createElement('div')
+                        card.classList.add('card')
+
+                        const h2 = document.createElement('h2')
+                        h2.textContent = asana.name
+                        card.appendChild(h2)
+
+                        const img = document.createElement('img')
+                        img.src = asana.image
+                        img.classList.add('asana-pic')
+                        card.appendChild(img)
+
+                        const info = document.createElement('p')
+                        info.classList.add('info')
+                        info.textContent = asana.info
+                        card.appendChild(info)
+
+                        const button = document.createElement('button')
+                        button.classList.add('learn-more-btn')
+                        button.id = asana.id
+                        button.textContent = "Let's Do It!"
+                        card.appendChild(button)
+
+                        button.addEventListener('click', () => {
+                            window.open(asana.link, '_blank')
+                        })
+
+                        document.getElementById('index').appendChild(card)
+
+                        closeMenu()
+                    })
+    
+                    menu.appendChild(asanaButton)
+                })
+            })
+            .catch(error => {
+                console.error('Error:', error)
+            })
     }
 })
 
